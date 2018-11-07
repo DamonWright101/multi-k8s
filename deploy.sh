231af766ddd3af25a0760e1d6aa074abbaffc1ea
Damon Wright <damonwright@damons-mbp.home>
2018-11-07 13:46:32 +0000

    added ingress instalation

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,9 @@ docker push dwright101/multi-client:$GIT_SHA
 docker push dwright101/multi-server:$GIT_SHA
 docker push dwright101/multi-worker:$GIT_SHA
 
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/mandatory.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/provider/cloud-generic.yaml
+
 kubectl apply -f k8s
 kubectl set image deployments/client-deployment client=dwright101/multi-client:$GIT_SHA
 kubectl set image deployments/server-deployment server=dwright101/multi-server:$GIT_SHA
